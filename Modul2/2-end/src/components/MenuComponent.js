4ef4b2d069b3f0cc0e6430e9824f0d9b4e27e5b5
Dragos Dehelean <34175492+dragosdehelean@users.noreply.github.com>
2018-11-21 14:59:51 +0200

    2.1

--- Modul2/2-end/src/components/MenuComponent.js
+++ Modul2/2-end/src/components/MenuComponent.js
@@ -12,17 +12,19 @@ class Menu extends Component {
             { 
               this.props.dishes.map(dish => {
                 return (
-                  <div key={dish.id} className="col-12 mt-5">
-                    <Media tag="li">
-                      <Media left middle>
-                        <Media object src={dish.image} alt={dish.name} />
-                      </Media>
-                      <Media body className="ml-5">
-                        <Media heading>{dish.name}</Media>
-                        <p>{dish.description}</p>
-                      </Media>
-                    </Media>
-                  </div>
+                  <Dish />
+                  
+                  // <div key={dish.id} className="col-12 mt-5">
+                  //   <Media tag="li">
+                  //     <Media left middle>
+                  //       <Media object src={dish.image} alt={dish.name} />
+                  //     </Media>
+                  //     <Media body className="ml-5">
+                  //       <Media heading>{dish.name}</Media>
+                  //       <p>{dish.description}</p>
+                  //     </Media>
+                  //   </Media>
+                  // </div>
                 );
               })
             }
