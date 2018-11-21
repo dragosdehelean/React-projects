eddf0a40814360097ea311674c7dfbccd81b28d3
Dragos Dehelean <34175492+dragosdehelean@users.noreply.github.com>
2018-11-21 14:58:48 +0200

    2.1

--- Modul2/2-end/src/App.js
+++ Modul2/2-end/src/App.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
+import React, { Component } from "react";
+import Header from "./components/HeaderComponent";
+import { DISHES } from './shared/dishes';
+import Menu from "./components/MenuComponent";
 
 
 class App extends Component {
-  render() {
+  state = { dishes: DISHES };
+
+  
+  render() {   
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu /> 
-      </div>
-     
+        {/* <Header /> */}
 
+        <Header dishesnumber = {this.state.dishes.length} />
+
+        <Menu dishes={this.state.dishes} />
+
+      </div>
     );
   }
 }
-
 
 export default App;
