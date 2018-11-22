55a3690ac3a8ef372602fe21c2cc46513548cd31
Dragos Dehelean <34175492+dragosdehelean@users.noreply.github.com>
2018-11-22 16:51:35 +0200

    modul 2.2

---
+++ Modul2.2/2.2-end/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import Header from "./components/HeaderComponent";
+import { DISHES } from './shared/dishes';
+import Menu from "./components/MenuComponent";
+
+
+class App extends Component {
+  state = { dishes: DISHES };
+
+  
+  render() {   
+    return (
+      <div>
+        {/* <Header /> */}
+
+        <Header dishesnumber = {this.state.dishes.length} />
+
+        <Menu dishes={this.state.dishes} />
+
+      </div>
+    );
+  }
+}
+
+export default App;
