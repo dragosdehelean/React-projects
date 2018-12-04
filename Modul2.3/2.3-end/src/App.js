e1b54d6a0f5cf4471aacd00ee9c09d79c1117323
Dragos Dehelean <34175492+dragosdehelean@users.noreply.github.com>
2018-12-04 17:08:54 +0200

    2.3

---
+++ Modul2.3/2.3-end/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import Header from "./components/Header";
+import { PLAYERS } from "./shared/players";
+import Player from "./components/Player";
+import NameForm from "./components/NameForm";
+
+class App extends Component {
+  state = { players: PLAYERS };
+
+  handleRemovePlayer = id => {
+    this.setState(prevState => {
+      return {
+        players: prevState.players.filter(p => p.id !== id)
+      };
+    });
+  };
+
+  handleAddPlayer = name => {
+    const newPlayer = { name: name, id: 5 };
+    this.setState(prevState => ({
+      players: [...prevState.players, newPlayer]
+    }));
+  };
+
+  removeHandler = id => {
+    this.setState(prevState => ({
+      players: prevState.players.filter(item => item.id !== id)
+    }));
+  };
+
+  render() {
+    return (
+      <div className="scoreboard">
+        <Header title="Scoreboard" totalPlayers={this.state.players.length} />
+
+        {/* Players list */}
+        {this.state.players.map(player => (
+          <Player
+            name={player.name}
+            id={player.id}
+            key={player.id.toString()}
+            removePlayer={this.handleRemovePlayer}
+          />
+        ))}
+
+        <NameForm addPlayer={this.handleAddPlayer} />
+      </div>
+    );
+  }
+}
+
+export default App;
+
---
+++ Modul2.3/2.3-end/src/shared/players.js
@@ -0,0 +1,19 @@
+export const PLAYERS = 
+[
+  {
+    name: "Guil",
+    id: 1
+  },
+  {
+    name: "Treasure",
+    id: 2
+  },
+  {
+    name: "Ashley",
+    id: 3
+  },
+  {
+    name: "James",
+    id: 4
+  }
+];
