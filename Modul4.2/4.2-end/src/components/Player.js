b82813afe2dfea961bdb19bf03c188293c603062
Dragos Dehelean <34175492+dragosdehelean@users.noreply.github.com>
2018-12-20 16:47:15 +0200

    4.2

---
+++ Modul4.2/4.2-end/src/components/Player.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Counter from "./Counter";
+
+const Player = props => {
+
+  console.log(props.name)
+
+  return (
+    <div className="player">
+      <span className="player-name">
+        <button className="remove-player" onClick={() => props.removePlayer(props.id)}>
+          ✖
+        </button>
+        {props.name}
+      </span>
+
+      <Counter 
+        score= {props.score}
+        changeScore = {props.changeScore} 
+        id = {props.id}
+        />
+    </div>
+  );
+};
+
+export default Player;
