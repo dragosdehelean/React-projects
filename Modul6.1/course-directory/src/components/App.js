ba4d31aa9e52123d95f0f930359d06e4c356fc85
Dragos Dehelean <dragos.dehelean@selenis.ro>
2019-02-13 02:16:15 +0200

    router

--- Modul6.1/course-directory/src/components/App.js
+++ Modul6.1/course-directory/src/components/App.js
@@ -5,7 +5,7 @@ import Home from './Home';
 const App = () => (
   <BrowserRouter>
     <div className="container">
-      <Route path="/" component={Home}/>
+      <Route path="/" component={Home} />
   </div>
   </BrowserRouter>
 );
