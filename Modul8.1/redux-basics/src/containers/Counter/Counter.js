927b917b40086f5e53e5b3c4e12d5fd28940e6dc
Dragos Dehelean <dragos.dehelean@selenis.ro>
2019-02-22 01:00:27 +0200

    redux

--- Modul8.1/redux-basics/src/containers/Counter/Counter.js
+++ Modul8.1/redux-basics/src/containers/Counter/Counter.js
@@ -1,33 +1,11 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import * as actionTypes from '../../store/actionTypes';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    counterChangedHandler = (action, value) => {
-        switch (action) {
-            case 'inc':
-                this.setState((prevState) => { return { counter: prevState.counter + 1 } })
-                break;
-            case 'dec':
-                this.setState((prevState) => { return { counter: prevState.counter - 1 } })
-                break;
-            case 'add':
-                this.setState((prevState) => { return { counter: prevState.counter + value } })
-                break;
-            case 'sub':
-                this.setState((prevState) => { return { counter: prevState.counter - value } })
-                break;
-            default:
-                return value;
-        }
-    }
-
     render() {
         return (
             <div>
@@ -36,6 +14,14 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrement} />
                 <CounterControl label="Add 5" clicked={this.props.onAdd} />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubstract} />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Save Results</button>
+                <ul className="results">
+                    {this.props.storedResults.map(result=>(
+                        <li onClick={()=>this.props.onDeleteResult(result.id)} key={result.id}>{result.value} (click to delete)</li>
+                    ))}                    
+                </ul>
+
             </div>
         );
     }
@@ -44,15 +30,18 @@ class Counter extends Component {
 const mapStateToProps = state => {
     return {
         ctr: state.counter,
+        storedResults: state.results
     }
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrement: ()=> dispatch({type: "INCREMENT"}),
-        onDecrement: ()=> dispatch({type: "DECREMENT"}),
-        onAdd: ()=> dispatch({type: "ADD", value: 5}),
-        onSubstract: ()=> dispatch({type: "SUBSTRACT", value: 5}),
+        onIncrement: ()=> dispatch({type: actionTypes.INCREMENT}),
+        onDecrement: ()=> dispatch({type: actionTypes.DECREMENT}),
+        onAdd: ()=> dispatch({type: actionTypes.ADD, value: 5}),
+        onSubstract: ()=> dispatch({type: actionTypes.SUBSTRACT, value: 5}),
+        onStoreResult: ()=>dispatch({type: actionTypes.STORE_RESULT}),
+        onDeleteResult: (id)=>dispatch({type: actionTypes.DELETE_RESULT, resultId: id}),
     }
 }
 
--- Modul8.1/redux-basics/src/store/reducer.js
+++ Modul8.1/redux-basics/src/store/reducer.js
@@ -1,21 +1,31 @@
+import * as actionTypes from './actionTypes';
+
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'INCREMENT':
+        case actionTypes.INCREMENT: 
             return { ...state, counter: state.counter + 1 };
-        case 'DECREMENT':
+        case actionTypes.DECREMENT:
             return { ...state, counter: state.counter - 1 };
-        case 'ADD':
+        case actionTypes.ADD:
             return { ...state, counter: state.counter + action.value };
-        case 'SUBSTRACT':
+        case actionTypes.SUBSTRACT:
             return { ...state, counter: state.counter - action.value };
+        case actionTypes.STORE_RESULT:
+            return { 
+                ...state,
+                results: [...state.results, {id: new Date(), value: state.counter}] };
+        case actionTypes.DELETE_RESULT:
+            return {...state,
+                results: state.results.filter( result => result.id !== action.resultId)
+            }
         default:
             return state;
     }
-
 }
 
 export default reducer;
